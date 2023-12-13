8f2e1e70b5a40a0841c8af7b81ccaaf197714347
mi-adesina <swiftyrange@gmail.com>
2023-12-13 02:25:25 +0100

    9. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-# islower - function to test if input is lower case
-# Return - True or False
+# islower - function to test if input is lower case.
+# c: character input.
+# Return - True or False.
 def islower(c):
     if ord(c) > 96 and ord(c) < 123:
         return True
