f07a7b002eed53a50ccf1f2b47d7a25cfc9ddbf7
mi-adesina <swiftyrange@gmail.com>
2023-12-16 04:24:25 +0100

    5. Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-from sys import argv as av
-if __name__ == '__main__':
-    ac = len(av) - 1
-    sum = 0
 
-    if ac > 0:
-        for i in range(1, len(av), 1):
-            sum += int(av[i])
-    print('{}'.format(sum))
+if __name__ == '__main__':
+
+    from variable_load_5 import a
+
+    print('{}'.format(a))
