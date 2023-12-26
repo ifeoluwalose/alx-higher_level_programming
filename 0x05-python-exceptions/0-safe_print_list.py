43a316b7d939428e9c0b85f0dd0d4b6dcf79f468
mi-adesina <swiftyrange@gmail.com>
2023-12-26 17:26:48 +0100

    1. Safe printing of an integers list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@ def safe_print_list(my_list=[], x=0):
     """safe_print_list: prints x elements of a list.
     Args:
         my_list:
-        x
+        :
 
     Returns:
-        None
+        Returns the real number of elements printed
     """
     i = 0
     try:
