c814d374bc98a518007c7e3c4d3411bf78c9a1d7
mi-adesina <swiftyrange@gmail.com>
2023-12-27 17:26:21 +0100

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,11 +16,11 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
            quotient = my_list_1[i] / my_list_2[i]
         except TypeError:
-            quotient = 0
             print("wrong type")
-        except IndexError:
             quotient = 0
+        except IndexError:
             print("out of range")
+            quotient = 0
         except ZeroDivisionError:
             print("division by 0")
             quotient = 0
