02a41762072baf5e074236d070cfc287c748de47
mi-adesina <swiftyrange@gmail.com>
2023-12-27 17:28:38 +0100

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,7 +14,7 @@ def list_division(my_list_1, my_list_2, list_length):
     output_list = []
     for i in range(list_length):
         try:
-           quotient = my_list_1[i] / my_list_2[i]
+            quotient = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             quotient = 0
