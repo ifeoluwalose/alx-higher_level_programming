645863a9ecf6acb19115c87a0f18de249858b37e
mi-adesina <swiftyrange@gmail.com>
2024-01-10 15:29:03 +0100

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """
         defines a square by size.
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
             Args:
                 size: the size of the square.
