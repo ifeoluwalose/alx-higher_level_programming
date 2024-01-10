60a13be792bb105db854924b09902f6fd5a81a11
mi-adesina <swiftyrange@gmail.com>
2024-01-10 15:25:00 +0100

    2. Size validation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defination of a class Square."""
+
+
+class Square:
+    """
+        defines a square by size.
+    """
+    def __init__(self, size):
+        """
+            Args:
+                size: the size of the square.
+        """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defination of a class Square."""
+
+
+class Square:
+    """
+        defines a square by size.
+    """
+    def __init__(self, size):
+        """
+            Args:
+                size: the size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
